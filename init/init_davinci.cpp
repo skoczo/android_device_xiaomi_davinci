fdef3347d20746882ea53aef28beb36fa21c98fc
skoczo <adrian.skoczylas@gmail.com>
2021-10-23 16:44:21 +0200

    safetynet fixes
    Change-Id: Ie250c4a7f53cf0c536bf5213b404cd177b7a4aa6

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -32,12 +32,29 @@
 #include <unistd.h>
 #include <vector>
 
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
+
+static const char *prop_key[] =
+        { "ro.boot.vbmeta.device_state", "ro.boot.verifiedbootstate", "ro.boot.flash.locked",
+          "ro.boot.veritymode", "ro.boot.warranty_bit", "ro.warranty_bit",
+          "ro.debuggable", "ro.secure", "ro.build.type", 
+          "ro.build.tags", "ro.vendor.boot.warranty_bit", "ro.vendor.warranty_bit",
+          "vendor.boot.vbmeta.device_state", "vendor.boot.verifiedbootstate", 
+          nullptr };
+
+static const char *prop_val[] =
+        { "locked", "green", "1",
+          "enforcing", "0", "0",
+          "0", "1", "user", 
+          "release-keys", "0", "0",
+          "locked", "green", 
+          nullptr };
 
 using android::base::GetProperty;
 using android::init::property_set;
@@ -58,15 +75,15 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "davinci-user 10 QKQ1.190825.002 V12.0.3.0.QFJCNXM release-keys",
-    "davinci-user 10 QKQ1.190825.002 V12.0.2.0.QFJMIXM release-keys",
+    "davinci_eea-user 10 QKQ1.190825.002 V12.0.4.0.QFJEUXM release-keys",
+    "davinci_eea-user 10 QKQ1.190825.002 V12.0.4.0.QFJEUXM release-keys",
     "davinciin-user 10 QKQ1.190825.002 V12.0.0.2.QFJINXM release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
     "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.3.0.QFJCNXM:user/"
     "release-keys",
-    "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V12.0.2.0.QFJMIXM:user/"
+    "Xiaomi/davinci_eea/davinci:10/QKQ1.190825.002/V12.0.4.0.QFJEUXM:user/"
     "release-keys",
     "Xiaomi/davinciin/davinciin:10/QKQ1.190825.002/V12.0.0.2.QFJINXM:user/"
     "release-keys",
@@ -77,15 +94,45 @@ constexpr const char *CLIENT_ID[] = {
     "android-xiaomi-rev1",
 };
 
+
 void property_override(char const prop[], char const value[], bool add = true) {
   prop_info *pi;
 
   pi = (prop_info *)__system_property_find(prop);
-  if (pi)
-    __system_property_update(pi, value, strlen(value));
-  else if (add)
-    __system_property_add(prop, strlen(prop), value, strlen(value));
+  if (pi) {
+    int result = __system_property_update(pi, value, strlen(value));
+    if(result != 0) {
+    	LOG(ERROR) << "Error during setting property " << prop;
+    } else {
+	    LOG(INFO) << "Success during setting property " << prop;
+    }
+  } else if (add) {
+    int result = __system_property_add(prop, strlen(prop), value, strlen(value));
+	if(result != 0) {
+    	LOG(ERROR) << "Error during setting property " << prop;
+    } else {
+	    LOG(INFO) << "Success during setting property " << prop;
+    }
+  }
 }
+
+
+void property_override_new(char const prop[], char const value[]) {
+  prop_info *pi;
+
+  pi = (prop_info *)__system_property_find(prop);
+  if (pi) {
+    int result = __system_property_update(pi, value, strlen(value));
+    if(result != 0) {
+    	LOG(ERROR) << "Error during setting property " << prop;
+    } else {
+	    LOG(INFO) << "Success during setting property " << prop;
+    }
+  } else {
+    LOG(ERROR) << "Not set property " << prop;
+  }
+}
+
 
 void load_props(const char *model, bool is_9t = false, bool is_in = false) {
   const auto ro_prop_override = [](const char *source, const char *prop,
@@ -137,6 +184,33 @@ void load_props(const char *model, bool is_9t = false, bool is_in = false) {
     ro_prop_override(nullptr, "com.google.clientidbase.ms", CLIENT_ID[1],
                      false);
   }
+  
+  for (int i = 0; prop_key[i]; ++i) {
+  	// ro override??
+  	// ro_prop_override
+    property_override(prop_key[i], prop_val[i], true);
+  }
+	ro_prop_override(nullptr, "ro.boot.vbmeta.device_state", "locked", false);
+	ro_prop_override(nullptr, "ro.boot.verifiedbootstate", "green", false);
+	ro_prop_override(nullptr, "ro.boot.flash.locked", "1", false);
+	ro_prop_override(nullptr, "ro.boot.veritymode", "enforcing", false);
+	ro_prop_override(nullptr, "ro.boot.warranty_bit", "0", false);
+	ro_prop_override(nullptr, "ro.warranty_bit", "0", false);
+	ro_prop_override(nullptr, "ro.debuggable", "0", false);
+	ro_prop_override(nullptr, "ro.secure", "1", false);
+	ro_prop_override(nullptr, "ro.build.type", "user", false);
+	ro_prop_override(nullptr, "ro.build.tags", "release-keys", false);
+	ro_prop_override(nullptr, "ro.vendor.boot.warranty_bit", "0", false);
+	ro_prop_override(nullptr, "ro.vendor.warranty_bit", "0", false);
+	property_override("vendor.boot.vbmeta.device_state", "locked", true);
+	property_override("vendor.boot.verifiedbootstate", "green", true);
+	
+	//ro_prop_override(nullptr, "ro.build.version.security_patch", "2021-01-01", false);
+	
+    property_override_new("ro.build.version.security_patch", "2021-01-01");
+    
+//    __system_property_update(pi, value, strlen(value))
+	
 }
 
 void vendor_load_properties() {
